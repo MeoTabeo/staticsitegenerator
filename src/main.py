8d82b828498d86003be8840eecb701163c4bd627
MeoTabeo <torsten@willitzkat.de>
2025-07-13 23:22:04 +0200

    batman

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from textnode import TextNode
+from textnode import TextType
+
+def main():
+    new_node = TextNode("Meo", TextType.ITALIC, "linknk")
+    print(new_node)
+
+
+
+if __name__ == "__main__":
+    main()
+
